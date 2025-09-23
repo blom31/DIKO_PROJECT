79c0792e04cdd90ae0ae998f0c0da56f5b69437e
Olga Betancourt <blom31@gmail.com>
2025-09-22 22:46:08 -0300

    desarrollo del lightbox en el albun venezuela-interior

--- js/venezuela.js
+++ js/venezuela.js
@@ -17,3 +17,13 @@ document.querySelectorAll(".close-album").forEach((btn) => {
     () => (btn.parentElement.style.display = "none")
   );
 });
+
+// Configuración de Lightbox
+lightbox.option({
+  resizeDuration: 200,
+  wrapAround: true, // permite navegar en bucle (última → primera)
+  alwaysShowNavOnTouchDevices: true,
+  disableScrolling: true, // bloquea scroll de la página mientras ves la foto
+  fadeDuration: 300,
+  imageFadeDuration: 300,
+});
